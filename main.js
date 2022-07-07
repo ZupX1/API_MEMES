12f4c7fa4293a7254ac6aae48fcada906b7d2fea
ZupX1 <vprevostcarpentier@normandiewebschool.fr>
2022-07-07 09:30:57 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,38 @@
+console.log("loaded");
+
+ function toBase64(r) {
+           
+            document.getElementById("imageD").src = r;
+            console.log(r);
+         }
+
+function gen()
+{
+    console.log("click");
+
+    let truc = fetch("https://v1.api.amethyste.moe/generate/3000years",
+    { 
+        method: "POST",
+        headers: 
+        {
+            'Authorization': "Bearer " + token.value,
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        },
+        body: new URLSearchParams({
+            'url': image.value,
+        })
+    });
+
+    truc.then(r => r.blob())
+    .then(r => {
+        let reader=new FileReader()
+        reader.addEventListener('loadend',()=>{
+        let contents=reader.result
+        toBase64(contents)
+        })
+        reader.readAsDataURL(r)
+    });
+
+}
+
+
